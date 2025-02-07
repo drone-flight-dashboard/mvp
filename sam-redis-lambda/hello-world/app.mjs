a7551e27335a6d97d9b835d9dc84bbe50683d594
Petr Popov <popov.pe@gmai.com>
2025-02-07 11:05:46 +0200

    lambda connects to redis

--- sam-redis-lambda/hello-world/app.mjs
+++ sam-redis-lambda/hello-world/app.mjs
@@ -10,16 +10,36 @@
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
+import Redis from 'ioredis';
+
+let client;
 
 export const lambdaHandler = async (event, context) => {
-    
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      })
-    };
+  if (!client) {
+    client = new Redis.Cluster(
+      [{ host: 'clustercfg.redis.ri1ocn.euc1.cache.amazonaws.com', port: 6379 }],
+      {
+        dnsLookup: (address, callback) => callback(null, address),
+        redisOptions: {
+          tls: {},
+        },
+      });
+  }
 
-    return response;
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'hello world ' + await client.get("key")}),
+    };
+  } catch (error) {
+    console.error("Redis connection error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error", error: error.message }),
+    };
+  } finally {
+    client.disconnect();
+  }
+};
+
   
